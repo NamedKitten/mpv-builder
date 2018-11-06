0a09173071378e4b10401240d1301e24b4d0d77f
Kitteh <kitteh@namedkitten.pw>
2018-11-06 10:32:58 +0000

    [Travis] sdl -> sdl2

--- build-deps.sh
+++ build-deps.sh
@@ -8,11 +8,6 @@ export CFLAGS="-Os -pipe"
 
 mkdir -p ~/.cache/deps
 
-if [[ "${USE_PREBUILT_MPV}" ]]; then 
-wget https://github.com/NamedKitten/mpv-builder/releases/download/continuous/deps.tar.xz
-sudo tar xvf deps.tar.xz -C /
-exit 0
-fi
 
 #rm -rf mpv-build
 git clone --depth 1 https://github.com/mpv-player/mpv-build mpv-build
@@ -22,7 +17,7 @@ export MPVDIR=`pwd`
 
 rm -rf ffmpeg mpv libass
 
-echo "--disable-programs --disable-runtime-cpudetect --disable-asm --disable-sdl --enable-small" > ffmpeg_options
+echo "--disable-programs --disable-runtime-cpudetect --disable-asm --disable-sdl2 --enable-small" > ffmpeg_options
 echo "--enable-libmpv-shared --prefix=/usr --disable-vapoursynth --enable-lgpl" > mpv_options
 echo "--disable-cplayer --disable-caca --disable-wayland --disable-gl-wayland --disable-libarchive  --disable-zlib --disable-tv --disable-debug-build --disable-manpage-build --disable-libsmbclient --disable-wayland --disable-sdl --disable-sndio --enable-plain-gl" >> mpv_options
 
@@ -37,6 +32,7 @@ if [[ "${UPLOAD}" ]]; then
 cd mpv
 python waf -v install --destdir=~/.cache/deps
 cd ~/.cache/deps
+rm -rf deps.tar.xz
 tar caf deps.tar.xz * 
 wget https://github.com/probonopd/uploadtool/raw/master/upload.sh
 bash upload.sh deps.tar.xz
